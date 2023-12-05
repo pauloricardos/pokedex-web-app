28c50ce619de358551535a50ea2e533b0f0d6612
Paulo Ricardo <paulorsscontato@hotmail.com>
2023-12-05 10:42:43 -0300

    test: add msw config

---
+++ app/tests/setup.ts
@@ -0,0 +1,7 @@
+import { beforeAll, afterAll, afterEach } from 'vitest';
+import { server } from './mocks/server';
+import '@testing-library/jest-dom/vitest';
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
