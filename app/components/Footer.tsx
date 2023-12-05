a3a60dad1984821d9d9f391b3372394276df360a
Paulo Ricardo <paulorsscontato@hotmail.com>
2023-12-05 10:44:51 -0300

    feat: create footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <p className="text-xs text-left text-gray-200 p-2">
+        made with ♡ by @pauloricardos on github
+      </p>
+    </footer>
+  );
+}
