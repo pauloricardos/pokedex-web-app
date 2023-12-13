8eb3874a5720acb9f94c2c8d869da77ff2f84ec3
Paulo Ricardo <paulorsscontato@hotmail.com>
2023-12-13 11:16:03 -0300

    test: add user-event package

--- app/tests/helpers/test-utils.ts
+++ app/tests/helpers/test-utils.ts
@@ -1,7 +1,8 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 export * from '@testing-library/react';
 
 const customRender = (ui: JSX.Element, options?: any) =>
   render(ui, { ...options });
 
-export { customRender as render };
+export { customRender as render, userEvent };
